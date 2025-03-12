5670b1382839dd646f81a361466bbdea111e4ae6
Tushar Patra <tushar04012002@gmail.com>
2025-03-12 13:49:57 +0530

    dsa

--- Medium/33. Search in Rotated Sorted Array.java
+++ Medium/33. Search in Rotated Sorted Array.java
@@ -1,79 +1,40 @@
 class Solution {
+
+ static int  searching(int[] nums,int target,int start,int end){
+
+    if(start>end) return -1;
+
+    int mid=start +(end-start)/2;
+
+    if(target==nums[mid]) return mid;
+
+    if(nums[start]<=nums[mid]){
+
+        if(target>=nums[start] && target <= nums[mid]){
+            return searching(nums,target,start,mid-1);
+        }
+
+        else return searching(nums,target,mid+1,end);
+    }
+
+    if(target>nums[mid] && target <=nums[end]){
+        return searching(nums,target,mid+1,end);
+    }
+
+    return searching(nums,target,start,mid-1);
+
+ }
+
     public int search(int[] nums, int target) {
 
-         int pivot=findPivot(nums);
-
-       // if you did not find a pivot, it means the array is not rotated
-
-        if(pivot==-1){
-
-            return binarySearch(nums,target,0,nums.length-1);
-
-        }
-
-
-        if(target==nums[pivot]){
-            return pivot;
-        }
-         if(target>=nums[0]){
-            return binarySearch(nums,target,0,pivot-1);
-        }
-
-         return binarySearch(nums,target,pivot+1,nums.length-1);
-        
-    }
-
-     static int binarySearch(int arr[],int target,int start,int end){
-
-        while(start<=end){
-
-            int mid=start+(end-start)/2;
-
-            if(target<arr[mid]){
-                end=mid-1;
-            }
-
-            else if(target>arr[mid]){
-                start=mid+1;
-            }
-
-            else{
-                return mid;
-            }
-
-
-        }
-
-        return -1;
-
-    }
-
-      // find the pivot
-
-    static int findPivot(int arr[]){
-
         int start=0;
-        int end=arr.length-1;
+        int end=nums.length-1;
 
-        while(start<=end) {
+        int ans=searching(nums,target,start,end);
 
-            int mid = start + (end - start) / 2;
+        return ans;
 
-            if (mid < end && arr[mid] > arr[mid + 1]) {
-                return mid;
-            }
-            else if (mid > start && arr[mid] < arr[mid - 1]) {
-                return mid - 1;
-            }
-            else if (arr[start] < arr[mid]) {
-                start = mid + 1;
-            }
-            else if (arr[start] >= arr[mid]) {
-                end = mid - 1;
-            }
 
-        }
-
-        return -1;
+        
     }
 }
