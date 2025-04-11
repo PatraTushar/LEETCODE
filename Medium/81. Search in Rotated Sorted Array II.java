1b3bdf397424f49474aa8ffc14d6154f38479fa1
Tushar Patra <tushar04012002@gmail.com>
2025-04-11 20:47:18 +0530

    d

--- Medium/81. Search in Rotated Sorted Array II.java
+++ Medium/81. Search in Rotated Sorted Array II.java
@@ -8,49 +8,39 @@ class Solution {
 
             int mid=start+(end-start)/2;
 
-            if(nums[mid]==target){
+            if(nums[mid]==target) return true;
 
-                return true;
+           else if(nums[start]==nums[mid] && nums[end]==nums[mid]){
+                    start++;
+                    end--;
+                
             }
 
-            if(nums[start]==nums[mid] && nums[mid]==nums[end]){
+            else if(nums[start]<=nums[mid]){
 
-                start++;
-                end--;
-                continue;
-            }
-
-            if(nums[start]<=nums[mid]){
-
-                if(target>=nums[start] && target<=nums[mid]){
-
+                if(target>=nums[start] && target<nums[mid]){
                     end=mid-1;
                 }
 
                 else{
-
                     start=mid+1;
                 }
+
             }
 
             else{
 
-                if(target>=nums[mid] && target<=nums[end]){
-
+                if(target>nums[mid] && target<=nums[end]){
                     start=mid+1;
                 }
 
                 else{
                     end=mid-1;
                 }
-            }  
-
-
+            }
         }
 
         return false;
-
-        
         
     }
 }
