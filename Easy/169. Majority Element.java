98bfc3f6c2b6cf539f90d40f7e0e71663b0fdd40
Tushar Patra <tushar04012002@gmail.com>
2025-04-18 19:55:53 +0530

    d

--- Easy/169. Majority Element.java
+++ Easy/169. Majority Element.java
@@ -1,18 +1,27 @@
 class Solution {
     public int majorityElement(int[] nums) {
 
-        HashMap<Integer,Integer> map=new HashMap<>();
-        for(int num:nums){
-            map.put(num,map.getOrDefault(num,0)+1);
-        }
+        int count=0;
+        int element=0;
 
-        for(int key:map.keySet()){
-            if(map.get(key)>nums.length/2){
-                return key;
+        for(int i=0;i<nums.length;i++){
+
+            if(count==0){
+
+                count=1;
+                element=nums[i];
+            }
+
+            else if(nums[i]==element){
+                count++;
+            }
+
+            else{
+                count--;
             }
         }
 
-      
-        return -1;
-    } 
+        return element;
+        
+    }
 }
