83c769856915f1bf5197ad5158e5c6755e498397
Tushar Patra <tushar04012002@gmail.com>
2025-04-18 21:48:52 +0530

    d

---
+++ Medium/53. Maximum Subarray.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+
+        int MAX=Integer.MIN_VALUE;
+        int sum=0;
+
+        for(int i=0;i<nums.length;i++){
+
+            sum+=nums[i];
+
+            if(sum>MAX){
+                MAX=sum;
+            }
+
+            if(sum<0){
+                sum=0;
+            }
+
+          
+        }
+
+        return MAX;
+        
+    }
+}
