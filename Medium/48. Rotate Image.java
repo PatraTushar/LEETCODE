fcea4451074d6f1203dafaffa77f0d110df99b29
Tushar Patra <tushar04012002@gmail.com>
2025-05-03 16:56:36 +0530

    d

--- Medium/48. Rotate Image.java
+++ Medium/48. Rotate Image.java
@@ -1,26 +1,39 @@
 class Solution {
     public void rotate(int[][] matrix) {
 
+          int rows=matrix.length;
+        int cols=matrix[0].length;
 
-// transpose 
-        for(int i=0;i<matrix.length;i++){
-            for(int j=i;j<matrix[0].length;j++){
+        for(int i=0;i<rows;i++){
+
+            for(int j=i;j<cols;j++){
+
                 int temp=matrix[i][j];
                 matrix[i][j]=matrix[j][i];
                 matrix[j][i]=temp;
+
             }
         }
 
 
-        // each row reverse
+        for(int i=0;i<rows;i++){
 
-        for(int i=0;i<matrix.length;i++){
-            for(int j=0;j<matrix.length/2;j++){
-                int temp=matrix[i][j];
-                matrix[i][j]=matrix[i][matrix.length-1-j];
-                matrix[i][matrix.length-1-j]=temp;
+            int start=0;
+            int end=cols-1;
+
+            while (start<=end){
+
+                int temp=matrix[i][start];
+                matrix[i][start]=matrix[i][end];
+                matrix[i][end]=temp;
+                start++;
+                end--;
             }
+
+
         }
+
+        
         
     }
 }
