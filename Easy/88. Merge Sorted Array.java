fa71a71dbdd5ec8e87302f835fbb1c04271549cd
Tushar Patra <tushar04012002@gmail.com>
2025-06-14 14:11:04 +0530

    dsa

--- Easy/88. Merge Sorted Array.java
+++ Easy/88. Merge Sorted Array.java
@@ -1,48 +1,34 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
 
-        int[] result=new int[m+n];
+    
+         int i=m-1;
+        int j=n-1;
+        int k=m+n-1;
 
-        int i=0;
-        int j=0;
-        int k=0;
+    
+        while (i>=0 && j>=0){
 
-        while(i<m && j<n){
-            if(nums1[i]<=nums2[j]){
-
-                result[k]=nums1[i];
-                i++;
-                k++;
-
-
+            if(nums1[i]>nums2[j]){
+                nums1[k]=nums1[i];
+                i--;
             }
 
-            else{
-                result[k]=nums2[j];
-                j++;
-                k++;
+            else {
+
+                nums1[k]=nums2[j];
+                j--;
             }
+            k--;
         }
 
-        
+        while(j>=0){
 
-        while(i<m){
-            result[k]=nums1[i];
-            i++;
-            k++;
+            nums1[k]=nums2[j];
+            j--;
+            k--;
+
         }
-
-        while(j<n){
-            result[k]=nums2[j];
-            j++;
-            k++;
-        }
-
-          for (int index = 0; index < m + n; index++) {
-            nums1[index] = result[index];
-        }
-
-        
         
     }
 }
