1bb7fed38a29cbe419f57a72fef41a03e284a1c3
Tushar Patra <tushar04012002@gmail.com>
2025-06-16 00:52:17 +0530

    dsa

--- Medium/1901. Find a Peak Element II.java
+++ Medium/1901. Find a Peak Element II.java
@@ -1,6 +1,10 @@
-class Solution {
+package ArraysbyKK.strivers;
 
-      static int findMaxInWhichRow(int[][] arr,int rows,int cols){
+import java.util.Arrays;
+
+public class  Q29 {
+
+    static int findMAxInWhichRow(int[][] arr, int rows, int cols){
 
         int max=Integer.MIN_VALUE;
         int row=-1;
@@ -10,51 +14,58 @@ class Solution {
             if(arr[i][cols]>max){
                 max=arr[i][cols];
                 row=i;
-
             }
+
+
+
         }
 
         return row;
 
 
     }
-    public int[] findPeakGrid(int[][] mat) {
 
-           int rows=mat.length;
-        int cols=mat[0].length;
+    static int[] peakElementII(int[][] arr){
+
         int low=0;
-        int high=cols-1;
+        int high=arr[0].length-1;
+        int rows=arr.length;
+        int cols=arr[0].length;
 
-
-        while (low<=high){
+        while (low <= high){
 
             int mid=low+(high-low)/2;
 
-            int rowHasMax=findMaxInWhichRow(mat,rows,mid);
+            int rowHasMax= findMAxInWhichRow(arr,rows,mid);
 
-            if((mid==0 || mat[rowHasMax][mid]>mat[rowHasMax][mid-1]) && (mid==cols-1 || mat[rowHasMax][mid]>mat[rowHasMax][mid+1])){
+            if((mid==0 || arr[rowHasMax][mid]>arr[rowHasMax][mid-1]) && (mid==cols-1 || arr[rowHasMax][mid] >arr[rowHasMax][mid+1])){
 
                 return new int[]{rowHasMax,mid};
 
             }
 
-            else if( mid>0 && mat[rowHasMax][mid-1]>mat[rowHasMax][mid]){
-                high=mid-1;
+            else if(arr[rowHasMax][mid]>arr[rowHasMax][mid+1]){
+                high=mid;
             }
 
             else {
                 low=mid+1;
             }
-
-
-
-
-
         }
 
         return new int[]{-1,-1};
+    }
 
 
-        
+
+    public static void main(String[] args) {
+
+        // peak element II
+
+        int[][] arr={{4,2,5,1,4,5},{2,9,3,2,3,2},{1,7,6,0,1,3},{3,6,2,3,7,2}};
+        int[] ans=peakElementII(arr);
+        System.out.println(Arrays.toString(ans));
+
+
     }
 }
