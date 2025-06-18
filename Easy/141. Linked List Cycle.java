e143458fe8c2fb5c18bbca0e6c5a4bc98cee0f7a
Tushar Patra <tushar04012002@gmail.com>
2025-06-18 23:44:30 +0530

    Merge completed

--- Easy/141. Linked List Cycle.java
+++ Easy/141. Linked List Cycle.java
@@ -1,38 +1,80 @@
-/**
- * Definition for singly-linked list.
- * class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode(int x) {
- *         val = x;
- *         next = null;
- *     }
- * }
- */
-public class Solution {
-    public boolean hasCycle(ListNode head) {
+package linkedListByKK.SinglyLinkedList.InterviewQuestion;
 
-        ListNode slow=head;
-        ListNode fast=head;
-        if(head==null) return false;
-        if(head.next==null) return false;
-    
-        while(fast!=null){
+public class Q11 {
 
-           if(slow==null) return false;
+    public static class Node{
+
+        int data;
+        Node next;
+
+        Node(int data){
+            this.data=data;
+        }
+    }
+
+    static boolean cycleInALinkedListI(Node head){
+
+        if(head==null || head.next==null) return false;
+
+
+        Node slow=head;
+        Node fast=head;
+
+        while (fast!=null && fast.next!=null){
+
+
             slow=slow.next;
-
-            if(fast.next==null) return false;
             fast=fast.next.next;
 
             if(slow==fast) return true;
+
         }
 
         return false;
 
 
 
+    }
 
-        
+    public static void main(String[] args) {
+
+        // Q: Cycle in a linked list I  (leeTCode-->141)
+
+        // Input 1
+        Node a =new Node(3);
+        Node b=new Node(2);
+        Node c=new Node(0);
+        Node d=new Node(-4);
+
+        a.next=b;
+        b.next=c;
+        c.next=d;
+        d.next=b;
+
+        System.out.println(cycleInALinkedListI(a));
+
+        // Input 2
+
+        Node e=new Node(1);
+        Node f=new Node(2);
+        e.next=f;
+        f.next=e;
+
+        System.out.println(cycleInALinkedListI(e));
+
+        // Input 3
+
+        Node g=new Node(1);
+        System.out.println(cycleInALinkedListI(g));
+
+
+
+
+
+
+
+
+
+
     }
 }
