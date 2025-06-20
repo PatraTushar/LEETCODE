970e86c8903dd2709efc3289188619c0a76e3ee7
Tushar Patra <tushar04012002@gmail.com>
2025-06-20 21:14:16 +0530

    dsa

--- Medium/92. Reverse Linked List II.java
+++ Medium/92. Reverse Linked List II.java
@@ -1,42 +1,104 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
-class Solution {
-    public ListNode reverseBetween(ListNode head, int left, int right) {
+package linkedListByKK.SinglyLinkedList.InterviewQuestion;
 
+public class Q27 {
 
-        ListNode Dummy=new ListNode(0);
-        Dummy.next=head;
-        ListNode t=Dummy;
-        ListNode temp=head;
+    public static class Node{
 
-        for(int i=0;i<left-1;i++){
-          t=t.next;
-          temp=temp.next;
+        int data;
+        Node next;
+        Node(int data){
+            this.data=data;
         }
 
-        ListNode curr=temp;
-        ListNode prev=null;
-        ListNode Agla=null;
+    }
 
-        for(int i=0;i<right-left+1;i++){
-            Agla=curr.next;
-            curr.next=prev;
-            prev=curr;
-            curr=Agla;
+    static void display(Node head){
+        Node temp=head;
+        while (temp!=null){
+            System.out.print(temp.data+" ");
+            temp=temp.next;
+        }
+        System.out.println();
+    }
+
+    static Node reverseBetween(Node head, int left, int right) {
+
+        if(head==null) return null;
+
+
+        Node pointer1 = head;
+
+
+
+        for (int i = 1; i < left-1 ; i++) {
+
+            pointer1 = pointer1.next;
+
         }
 
-        t.next=prev;
-        temp.next=curr;
+        Node pointer2;
 
-        return Dummy.next;
-        
+        if(left==1){
+            pointer2=pointer1;
+        }
+
+        else {
+            pointer2=pointer1.next;
+        }
+
+
+
+
+        Node prev = null;
+        Node curr = pointer2;
+        Node Agla;
+
+
+        for (int i = 1; i <= right-left + 1; i++) {
+
+            Agla = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = Agla;
+
+        }
+
+        if(left==1){
+            pointer1.next=curr;
+            return prev;
+        }
+        else {
+
+            pointer1.next = prev;
+            pointer2.next = curr;
+        }
+
+
+        return head;
+
+
+
+
+
+    }
+
+    public static void main(String[] args) {
+
+        // Q: reverse linked list II (leeTCode->92)
+
+        Node a=new Node(1);
+        Node b=new Node(2);
+        Node c=new Node(3);
+        Node d=new Node(4);
+        Node e=new Node(5);
+        a.next=b;
+        b.next=c;
+        c.next=d;
+        d.next=e;
+        display(a);
+
+        Node Ans=reverseBetween(a,2,4);
+        display(Ans);
+
     }
 }
