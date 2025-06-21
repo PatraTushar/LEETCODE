c7e343615bd1064e5e98aae6329427d2762e3c8b
Tushar Patra <tushar04012002@gmail.com>
2025-06-21 14:33:02 +0530

    dsa

--- Medium/19. Remove Nth Node From End of List.java
+++ Medium/19. Remove Nth Node From End of List.java
@@ -1,42 +1,84 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
-class Solution {
-    public ListNode removeNthFromEnd(ListNode head, int n) {
+package linkedListByKK.SinglyLinkedList.InterviewQuestion;
 
-       if(head.next==null) return null;;
 
-        ListNode temp=head;
-        int length=0;
+public class Q30 {
 
-        while(temp!=null){
-            temp=temp.next;
-            length++;
+   public static class Node{
+
+        int data;
+        Node next;
+
+        Node(int data){
+            this.data=data;
+        }
+    }
+
+    static void display(Node head){
+
+       Node temp=head;
+       while (temp!=null){
+           System.out.print(temp.data +" ");
+           temp=temp.next;
+       }
+        System.out.println();
+    }
+
+
+    public static Node removeNthFromEnd(Node head, int n) {
+
+
+        if(head==null) return null;
+
+        Node slow=head;
+        Node fast=head;
+
+
+        for(int i=0;i<n;i++){
+
+            fast=fast.next;
+
         }
 
-        if(length==n) return head.next;
+        if(fast==null) return head.next;
 
-        temp=head;
-
-        int t=length-n;
-
-        for(int i=0;i<t-1;i++){
-
-         temp=temp.next;
-           
+        while (fast.next!=null){
+            slow=slow.next;
+            fast=fast.next;
         }
 
-        temp.next=temp.next.next;
+        if(slow.next==null) return head;
+        slow.next=slow.next.next;
+
 
 
         return head;
-        
+
+
+
+    }
+
+
+
+    public static void main(String[] args) {
+
+        // Q: Reverse nth node from end of the list (leeTCode->19)
+
+        Node a=new Node(1);
+        Node b=new Node(2);
+        Node c=new Node(3);
+        Node d=new Node(4);
+        Node e=new Node(5);
+
+        a.next=b;
+        b.next=c;
+        c.next=d;
+        d.next=e;
+
+        display(a);
+        Node ans=removeNthFromEnd(a,4);
+        display(ans);
+
+
+
     }
 }
