ba03d09dbf822c415541cccdbbdebcd6c57f3646
Tushar Patra <tushar04012002@gmail.com>
2025-06-24 01:05:20 +0530

    dsa

--- Medium/2095. Delete the Middle Node of a Linked List.java
+++ Medium/2095. Delete the Middle Node of a Linked List.java
@@ -1,31 +1,74 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
-class Solution {
-    public ListNode deleteMiddle(ListNode head) {
+package linkedListByKK.SinglyLinkedList.InterviewQuestion;
 
-        ListNode slow=head;
-        ListNode fast=head;
+public class Q35 {
 
-        if(head.next==null){
-            return null;
+    public static class ListNode {
+
+        int data;
+        ListNode next;
+
+        ListNode(int data) {
+            this.data = data;
+        }
+    }
+
+    static ListNode deleteMiddle(ListNode head) {
+
+        //Time Complexity: O(n)
+        //Space Complexity: O(1)
+
+        // for even right middle
+
+         if(head.next==null) return null;
+
+
+        ListNode slow = head;
+        ListNode fast = head;
+
+        if(fast==null) return null;
+        if (fast.next==null) return head;
+        fast=fast.next.next;
+
+        while (fast != null && fast.next != null ) {
+            slow = slow.next;
+            fast = fast.next.next;
         }
 
-        while( fast.next.next!=null && fast.next.next.next!=null){
+        if (slow != null && slow.next != null) slow.next = slow.next.next;
 
-            slow=slow.next;
-            fast=fast.next.next;
-        }
-
-        slow.next=slow.next.next;
         return head;
-        
+    }
+
+    static void display(ListNode head) {
+
+        ListNode temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+        }
+        System.out.println();
+    }
+
+
+    public static void main(String[] args) {
+
+        ListNode a = new ListNode(1);
+        ListNode b = new ListNode(2);
+        ListNode c = new ListNode(3);
+        ListNode d = new ListNode(4);
+        ListNode e = new ListNode(5);
+        ListNode f = new ListNode(6);
+
+        a.next = b;
+        b.next = c;
+        c.next = d;
+        d.next = e;
+        e.next = f;
+
+        display(a);
+        ListNode Ans = deleteMiddle(a);
+        display(Ans);
+
+
     }
 }
