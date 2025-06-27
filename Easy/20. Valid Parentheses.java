a96e2322f12dd73d65bd61cb34533d7acb29e6d3
Tushar Patra <tushar04012002@gmail.com>
2025-06-27 20:13:14 +0530

    dsa

--- Easy/20. Valid Parentheses.java
+++ Easy/20. Valid Parentheses.java
@@ -1,36 +1,39 @@
 class Solution {
     public boolean isValid(String s) {
+
         
-         Stack<Character> st=new Stack();
+        int i = 0;
+        int n = s.length();
 
-         for(int i=0;i<s.length();i++){
-            char ch=s.charAt(i);
+        Stack<Character> st = new Stack<>();
 
-            if(ch=='(' || ch=='{' || ch=='['){
+        while (i < n) {
+
+            char ch = s.charAt(i);
+
+            if (ch == '{' || ch == '(' || ch == '[') {
+
                 st.push(ch);
+            }
+            else {
+
+                if(st.isEmpty()) return false;
+
+                char top=st.peek();
+
+                if (ch == ')' && top == '(' || ch == '}' && top == '{' || ch == ']' && top == '[')
+                    st.pop();
+
+                else return false;
+
 
             }
+            i++;
 
-            else if(ch==')'){
 
-                if(st.size()==0 || st.pop()!='(')
-                return false;
-            }
+        }
 
-             else if(ch=='}'){
-
-                if(st.size()==0 || st.pop()!='{')
-                return false;
-            }
-
-             else if(ch==']'){
-
-                if(st.size()==0 || st.pop()!='[')
-                return false;
-            }
-         }
-
-         return st.isEmpty();
+        return st.isEmpty();
         
     }
 }
